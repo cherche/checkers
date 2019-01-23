bf5a8df8a1303bc5659a3ace56d9a6c94dda66a2
Ryan Nguyen <contactryannguyen@gmail.com>
2019-01-22 19:46:19 -0500

    Added gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,25 @@ const cleanCSS = require('gulp-clean-css')
 const webpack = require('webpack-stream')
 const babel = require('gulp-babel')
 const minify = require('gulp-babel-minify')
+
+gulp.task('css', () => {
+  return gulp.src(['src/css/main.css'])
+    .pipe(concat('bundle.css'))
+    .pipe(cleanCSS({ compatibility: '*' }))
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('js', () => {
+  return gulp.src('src/index.js')
+    .pipe(webpack({
+      // watch: true,
+      output: { filename: 'bundle.js' }
+    }))
+    .pipe(babel({
+      presets: ['env']
+    }))
+    .pipe(minify())
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('default', gulp.parallel('css', 'js'))
